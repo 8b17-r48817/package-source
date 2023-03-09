df9eaac7645557eac03e17d59b66ddaa6be2bf77
D3vil0p3r <crazysnob@live.it>
2023-03-09 19:00:28 +0100

    Update run-once.sh

--- packages/athena-application-config/etc/profile.d/run-once.sh
+++ packages/athena-application-config/etc/profile.d/run-once.sh
@@ -2,7 +2,7 @@
 
 detect_virt=$(systemd-detect-virt)
 
-if [ ! -z $DISPLAY ] ||  ([ $detect_virt != "docker" ] && [ $detect_virt != "podman" ] && [ $detect_virt != "wsl" ]); then
+if ([ "$DISPLAY" != ":0" ] && [ "$detect_virt" != "wsl" ]) || ([ "$detect_virt" != "docker" ] && [ "$detect_virt" != "podman" ] && [ "$detect_virt" != "wsl" ]); then
 
      FLAGFILE="$HOME/.flag-work-once"
      #FLAGNET="$HOME/.flag-net-once"
