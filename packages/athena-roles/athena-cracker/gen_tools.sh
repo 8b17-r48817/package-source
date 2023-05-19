b8ffc51fa0720c0a8aed005daf41d9c7b7738ea9
D3vil0p3r <>
2023-05-19 11:40:50 +0200

    Added psudohash

--- packages/athena-roles/athena-cracker/gen_tools.sh
+++ packages/athena-roles/athena-cracker/gen_tools.sh
@@ -8,4 +8,5 @@ sed -i '/^$/d' PKGBUILD #Delete all empty lines
 printf '\n' >> PKGBUILD #Avoid that the new "depends" string is placed on an existing row
 printf 'depends=( ' >> PKGBUILD
 printf "'%s' " $(pacman -Sgq blackarch-cracker blackarch-crypto | awk '!seen[$0]++' | sed -e "$(sed 's:.*:s/^&$//ig:' <<<$(printf "%s\n" "${to_exclude[@]}"))") >> PKGBUILD
+printf "'psudohash' " >> PKGBUILD
 printf ')\n' >> PKGBUILD
