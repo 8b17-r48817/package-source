5421fd35abeed4949987c1e4e7423a630b6099d2
D3vil0p3r <>
2023-05-21 12:26:37 +0200

    Akame background changed

--- packages/athena-akame-theme/build.sh
+++ packages/athena-akame-theme/build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+pkgname=$(grep "^pkgname=" PKGBUILD | awk -F"=" '{print $2}')
+pkgrel=$(grep "^pkgrel=" PKGBUILD | awk -F"=" '{split($2,a," ");gsub(/"/, "", a[1]);print a[1]}')
+arch=$(grep "^arch=" PKGBUILD | awk -F"'" '{print $2}')
+
 #NEED ONLY TO EDIT  sourcefiles VARIABLE
 
 sourcefiles="icons"
@@ -11,11 +15,7 @@ updpkgsums
 #makepkg -g >> PKGBUILD
 makepkg -f -scr --sign
 
-pkgname=$(grep "^pkgname=" PKGBUILD | awk -F"=" '{print $2}')
 pkgver=$(grep "^pkgver=" PKGBUILD | awk -F"=" '{print $2}')
-pkgrel=$(grep "^pkgrel=" PKGBUILD | awk -F"=" '{split($2,a," ");gsub(/"/, "", a[1]);print a[1]}')
-arch=$(grep "^arch=" PKGBUILD | awk -F"'" '{print $2}')
-
 pkgfile=$pkgname-$pkgver-$pkgrel-$arch.pkg.tar.zst
 rm -rf src pkg $pkgname.tar.gz Bibata-Modern-DarkRed.tar.gz rtl88-Themes Nightfox-GTK-Theme
 rm -rf ../../$pkgfile ../../$pkgfile.sig
