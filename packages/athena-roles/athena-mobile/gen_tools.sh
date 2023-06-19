98cb1dd358679755e141a63cfb8a390a7e015913
D3vil0p3r <>
2023-06-20 00:12:41 +0200

    Fixed wireplumber conflict Calamares

--- packages/athena-roles/athena-mobile/gen_tools.sh
+++ packages/athena-roles/athena-mobile/gen_tools.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-to_exclude=("apkid" "mobsf" "quickscope" "smartphone-pentest-framework" "radare2-keystone" "sigploit")
+to_exclude=("apkid" "mobsf" "quickscope" "smartphone-pentest-framework" "radare2-keystone" "sigploit" "pwntools")
 
 sed -i '/depends=/d' PKGBUILD
 sed -i '/^$/d' PKGBUILD #Delete all empty lines
@@ -8,4 +8,5 @@ sed -i '/^$/d' PKGBUILD #Delete all empty lines
 printf '\n' >> PKGBUILD #Avoid that the new "depends" string is placed on an existing row
 printf 'depends=( ' >> PKGBUILD
 printf "'%s' " $(pacman -Sgq blackarch-mobile blackarch-reversing | awk '!seen[$0]++' | sed -e "$(sed 's:.*:s/^&$//ig:' <<<$(printf "%s\n" "${to_exclude[@]}"))") >> PKGBUILD
+printf "'python-pwntools' " >> PKGBUILD
 printf ')\n' >> PKGBUILD
