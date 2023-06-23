fe99d5e3c04e3ae9e26eba2dc49ec72d51e6a848
D3vil0p3r <>
2023-06-23 13:55:17 +0200

    Fixed athena-roles

--- packages/athena-roles/athena-blackhat/gen_tools.sh
+++ packages/athena-roles/athena-blackhat/gen_tools.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-to_exclude=("apkid" "python-yara-rednaga" "ssma" "mobsf" "quickscope" "argus-clients" "qt3" "xplico" "pyqt3" "qscintilla-qt3" "faradaysec" "gerix-wifi-cracker" "inguma" "braces" "facebrok" "pfff" "fuzzdb" "smartphone-pentest-framework" "trape" "sagan" "firefox-security-toolkit" "binflow" "fastnetmon" "radare2-keystone" "tyton" "vpnpivot" "wolpertinger" "sigploit" "ncpfs" "pwntools" "adenum" "formatstringexploiter" "peach-fuzz" "zeratool")
+to_exclude=("apkid" "python-yara-rednaga" "ssma" "mobsf" "quickscope" "argus-clients" "qt3" "xplico" "pyqt3" "qscintilla-qt3" "faradaysec" "gerix-wifi-cracker" "inguma" "braces" "facebrok" "pfff" "fuzzdb" "smartphone-pentest-framework" "trape" "sagan" "firefox-security-toolkit" "binflow" "fastnetmon" "radare2-keystone" "tyton" "vpnpivot" "wolpertinger" "sigploit" "ncpfs" "pwntools")
 
 sed -i '/depends=/d' PKGBUILD
 sed -i '/^$/d' PKGBUILD #Delete all empty lines
