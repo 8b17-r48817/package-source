911688f941dc1279a82070a64693b85c303d56fc
D3vil0p3r <>
2023-07-07 15:25:17 +0200

    Removed cyberchef because it is on Chaotic

--- packages/cyberchef-electron/prompt.js
+++
@@ -1,6 +0,0 @@
-// put this preload for main-window to give it prompt()
-const ipcRenderer = require('electron').ipcRenderer;
-
-window.prompt = function(title, val) {
-    return ipcRenderer.sendSync('prompt', {title, val})
-};
