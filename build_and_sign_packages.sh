bb0545f3b0938eb949bccda42120fa19d04cb648
D3vil0p3r <crazysnob@live.it>
2023-07-21 16:50:32 +0200

    Update build_and_sign_packages.sh

--- build_and_sign_packages.sh
+++ build_and_sign_packages.sh
@@ -19,10 +19,10 @@ build_and_sign_packages() {
         echo -e "\nBuilding and signing packages in $pkg_dir..."
         
         updpkgsums
-        pkgname=$(grep "^pkgname=" PKGBUILD | awk -F"=" '{print $2}')
-        pkgrel=$(grep "^pkgrel=" PKGBUILD | awk -F"=" '{split($2,a," ");gsub(/"/, "", a[1]);print a[1]}')
-        arch=$(grep "^arch=" PKGBUILD | awk -F"'" '{print $2}')
-        pkgver=$(grep "^pkgver=" PKGBUILD | awk -F"=" '{print $2}')
+        pkgname=$(grep "^pkgname=" $pkg_dir | awk -F"=" '{print $2}')
+        pkgrel=$(grep "^pkgrel=" $pkg_dir | awk -F"=" '{split($2,a," ");gsub(/"/, "", a[1]);print a[1]}')
+        arch=$(grep "^arch=" $pkg_dir | awk -F"'" '{print $2}')
+        pkgver=$(grep "^pkgver=" $pkg_dir | awk -F"=" '{print $2}')
         pkgfile=$pkgname-$pkgver-$pkgrel-$arch.pkg.tar.zst
 
         makepkg -f -scr
