06f9a1ff88f4b0cb11614127a7dff0ff7a6ce2ca
D3vil0p3r <>
2023-07-22 04:19:07 +0200

    Fixed all build.sh

--- packages/athena-calamares/build.sh
+++ packages/athena-calamares/build.sh
@@ -3,13 +3,12 @@ echo "################################################################"
 echo "################### Making the package"
 echo "################################################################"
 makepkg -f -scr --noconfirm
-yes| paru -c
-mv *.pkg.tar.zst* ../../
+#yes| paru -c
 echo "################################################################"
 echo "################### Moved the .zst file to x86_64 folder"
 echo "################################################################"
 find . -type d -not -path "." -exec sudo rm -r "{}" \;
-find . -type f -not -name "build.sh" -not -name "*.install" -not -name PKGBUILD -not -name  "49-nopasswd-calamares.rules" -not -name "calamares_polkit" -not -name "calamares.desktop" -not -name "calamares-3.2.49.1" -not -name "*.pkg.tar.zst" -delete
+find . -type f -not -name "build.sh" -not -name "*.install" -not -name PKGBUILD -not -name  "49-nopasswd-calamares.rules" -not -name "calamares_polkit" -not -name "calamares.desktop" -not -name "*.pkg.tar.zst*" -delete
 echo "################################################################"
 echo "################### Removed all redundant files and folders"
 echo "################################################################"
