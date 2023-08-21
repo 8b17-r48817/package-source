abaf296321e9c3c38d59acec2db470cb02507025
D3vil0p3r <>
2023-08-21 04:01:36 +0200

    Added htb theme

--- packages/athena-htb-theme/build.sh
+++ packages/athena-htb-theme/build.sh
@@ -6,7 +6,7 @@ arch=$(grep "^arch=" PKGBUILD | awk -F"'" '{print $2}')
 
 #NEED ONLY TO EDIT  sourcefiles VARIABLE
 
-sourcefiles="HackTheBox icons"
+sourcefiles="HackTheBox HackTheBox-GTK-Theme icons"
 
 #sed -i -e '/^sha256/d' -e '/^sha512/d' PKGBUILD
 
@@ -17,5 +17,5 @@ makepkg -f -scr --noconfirm
 
 pkgver=$(grep "^pkgver=" PKGBUILD | awk -F"=" '{print $2}')
 pkgfile=$pkgname-$pkgver-$pkgrel-$arch.pkg.tar.zst
-rm -rf src pkg $pkgname.tar.gz Bibata-Modern-DarkRed.tar.gz rtl88-Themes Nightfox-GTK-Theme
+rm -rf src pkg $pkgname.tar.gz Afterglow-Cursors-Recolored
 
