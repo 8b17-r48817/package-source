f70a89bb635afa9daf0b0e0c0419e5fe9894437c
D3vil0p3r <>
2023-09-02 02:51:36 +0200

    Test browser-check

--- push.sh
+++ push.sh
@@ -18,15 +18,16 @@ len=$((${#arr_pkg[@]} - 1))
 
 for i in $(seq 0 $len)
 do
-    #echo ${arr_pkg[$i]}
+    ##echo ${arr_pkg[$i]}
     filename=${arr_pkg[$i]}
     replace=$(sed -r 's/(\b[0-9]{1,3}\.){2}[0-9]{1,2}-[0-9]{1,2}\b'/"\*"/ <<<$filename) # find string with a number with 1 or 2 or 3 digits followed by . for two times, and then a number of 2 digits with - and another number of 2 digits. It will replace <pkgname>-1.0.1-1.any.pkg.zst by <pkgname>-*.any.pkg.zst
-    rm -rf $repo_path/x86_64/$replace $repo_path/x86_64/$replace.sig $repo_path/aarch64/$replace $repo_path/aarch64/$replace.sig
+    #rm -rf $repo_path/x86_64/$replace $repo_path/x86_64/$replace.sig $repo_path/aarch64/$replace $repo_path/aarch64/$replace.sig
+    rm -rf $repo_path/x86_64/$replace $repo_path/x86_64/$replace.sig
     echo -e "Type the passphrase to sign the package:"
     gpg --detach-sign --use-agent --pinentry-mode loopback --passphrase --passphrase-fd 0 --output $filename.sig $filename
     cp -rf $filename $filename.sig $repo_path/x86_64/
-    cp -rf $filename $filename.sig $repo_path/aarch64/
-    #rm -rf $filename $filename.sig
+    #cp -rf $filename $filename.sig $repo_path/aarch64/
+    ##rm -rf $filename $filename.sig
     echo "$filename package moved to $repo_path repository."
 done
 
