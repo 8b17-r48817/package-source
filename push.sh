b3648cd7ce68cbf9a322640b9f69e05142c7b253
D3vil0p3r <>
2023-09-02 15:01:26 +0200

    Fix browser-check

--- push.sh
+++ push.sh
@@ -22,7 +22,8 @@ do
     filename=${arr_pkg[$i]}
     replace=$(sed -r 's/(\b[0-9]{1,3}\.){2}[0-9]{1,2}-[0-9]{1,2}\b'/"\*"/ <<<$filename) # find string with a number with 1 or 2 or 3 digits followed by . for two times, and then a number of 2 digits with - and another number of 2 digits. It will replace <pkgname>-1.0.1-1.any.pkg.zst by <pkgname>-*.any.pkg.zst
     #rm -rf $repo_path/x86_64/$replace $repo_path/x86_64/$replace.sig $repo_path/aarch64/$replace $repo_path/aarch64/$replace.sig
-    rm -rf $repo_path/x86_64/$replace $repo_path/x86_64/$replace.sig
+    echo "$replace"
+    rm -rf ./$filename.sig $repo_path/x86_64/$replace $repo_path/x86_64/$replace.sig
     echo -e "Type the passphrase to sign the package:"
     gpg --detach-sign --use-agent --pinentry-mode loopback --passphrase --passphrase-fd 0 --output $filename.sig $filename
     cp -rf $filename $filename.sig $repo_path/x86_64/
