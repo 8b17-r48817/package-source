8fd0fc3a314ce3000123c77c88db2e0d1bbb4a70
D3vil0p3r <>
2023-09-19 23:07:07 +0200

    Added kitty themes directly as source of themes

--- packages/athena-xxe-theme/build.sh
+++ packages/athena-xxe-theme/build.sh
@@ -17,5 +17,5 @@ makepkg -f -scr --noconfirm
 
 pkgver=$(grep "^pkgver=" PKGBUILD | awk -F"=" '{print $2}')
 pkgfile=$pkgname-$pkgver-$pkgrel-$arch.pkg.tar.zst
-rm -rf src pkg $pkgname.tar.gz
+rm -rf src pkg $pkgname.tar.gz gruvbox-dark.conf
 
